6cc255f4ced64f331f162fe1f20e1c65026ed595
youwol-guillaume <greinisch@youwol.com>
2024-11-19 16:54:08 +0100

    ✅ Setup `jest.config.ts`

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,14 @@
 import { Config } from 'jest'
 
+const reporters = ['default', 'jest-junit']
+
 const jestConfig: Config = {
-    preset: '@youwol/jest-preset',
-    modulePathIgnorePatterns: [],
+    transform: { '^.+\\.tsx?$': ['ts-jest', {}] },
+    testRunner: 'jest-circus',
+    testEnvironment: 'jsdom',
+    reporters,
+    modulePathIgnorePatterns: ['<rootDir>/.w3nest', '<rootDir>/dist'],
     testPathIgnorePatterns: ['rx-vdom-doc'],
+    //testEnvironmentOptions: { url: `${url_scheme}://${url_host}:${url_port}` },
 }
 export default jestConfig
