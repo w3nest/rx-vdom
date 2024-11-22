420bb194846d0720ef06e1016b16673eed792008
youwol-guillaume <greinisch@youwol.com>
2024-11-22 08:10:11 +0100

    ✨ Add type inference helpers of `RxAttr`, `RxChild`, `RxChildren`

--- src/lib/type-utils.ts
+++ src/lib/type-utils.ts
@@ -4,6 +4,14 @@
  * @module
  */
 
+import {
+    AnyHTMLAttribute,
+    AnyVirtualDOM,
+    RxAttribute,
+    RxChild,
+    RxChildren,
+} from './api'
+
 /**
  * Check whether 2 types are equals.
  *
@@ -36,3 +44,89 @@ export type WritableKeysOf<T> = {
  * @template T type to transform
  */
 export type WritablePart<T> = Pick<T, WritableKeysOf<T>>
+
+/**
+ * Type inference helper for constructing an {@link RxAttribute}.
+ * This function has no runtime behavior beyond directly returning the input
+ * argument.
+ *
+ * Use this helper to infer types when defining reactive attributes.
+ *
+ * @template TDomain The domain type associated with the attribute.
+ * @template Target The HTML attribute type to which this applies.
+ * @param attr The reactive attribute definition.
+ * @returns The provided attribute definition.
+ */
+export function attr$<TDomain, Target extends AnyHTMLAttribute>(
+    attr: RxAttribute<TDomain, Target>,
+) {
+    return attr
+}
+
+/**
+ * Type inference helper for constructing an {@link RxChild}.
+ * This function has no runtime behavior beyond directly returning the input
+ * argument.
+ *
+ * Use this helper to infer types when defining reactive child elements.
+ *
+ * @template TDomain The domain type associated with the child.
+ * @template TVDom The virtual DOM type of the child.
+ * @param child The reactive child definition.
+ * @returns The provided child definition.
+ */
+export function child$<TDomain, TVDom extends AnyVirtualDOM>(
+    child: RxChild<TDomain, TVDom>,
+) {
+    return child
+}
+/**
+ * Type inference helper for constructing {@link RxChildren} with the 'replace'
+ * policy.
+ * This function has no runtime behavior beyond directly returning the input
+ * argument.
+ *
+ * Use this helper to infer types when defining reactive children with the
+ * 'replace' policy.
+ *
+ * @template TDomain The domain type associated with the children.
+ * @param children The reactive children definition with the 'replace' policy.
+ * @returns The provided children definition.
+ */
+export function replace$<TDomain>(children: RxChildren<'replace', TDomain>) {
+    return children
+}
+
+/**
+ * Type inference helper for constructing {@link RxChildren} with the 'sync'
+ * policy.
+ * This function has no runtime behavior beyond directly returning the input
+ * argument.
+ *
+ * Use this helper to infer types when defining reactive children with the
+ * 'sync' policy.
+ *
+ * @template TDomain The domain type associated with the children.
+ * @param children The reactive children definition with the 'sync' policy.
+ * @returns The provided children definition.
+ */
+export function sync$<TDomain>(children: RxChildren<'sync', TDomain>) {
+    return children
+}
+
+/**
+ * Type inference helper for constructing {@link RxChildren} with the 'append'
+ * policy.
+ * This function has no runtime behavior beyond directly returning the input
+ * argument.
+ *
+ * Use this helper to infer types when defining reactive children with the
+ * 'append' policy.
+ *
+ * @template TDomain The domain type associated with the children.
+ * @param children The reactive children definition with the 'append' policy.
+ * @returns The provided children definition.
+ */
+export function append$<TDomain>(children: RxChildren<'append', TDomain>) {
+    return children
+}
