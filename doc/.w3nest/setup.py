4bd1275dad5f4a4bdfa42333ee14e163474684b9
youwol-guillaume <greinisch@youwol.com>
2024-12-19 10:53:57 +0100

    🚨 [doc-app] apply `black`

--- doc/.w3nest/setup.py
+++ doc/.w3nest/setup.py
@@ -1,22 +1,29 @@
 from pathlib import Path
 from shutil import copyfile
 
-from w3nest.ci.ts_frontend import PackageType, Dependencies, \
-    RunTimeDeps, DevServer, Bundles, MainModule, ProjectConfig
+from w3nest.ci.ts_frontend import (
+    PackageType,
+    Dependencies,
+    RunTimeDeps,
+    DevServer,
+    Bundles,
+    MainModule,
+    ProjectConfig,
+)
 from w3nest.ci.ts_frontend.regular import generate_template
 from w3nest.utils import parse_json
 
 
 project_folder = Path(__file__).parent.parent
 
-pkg_json = parse_json(project_folder / 'package.json')
-pkg_json_rxvdom = parse_json(project_folder / '..' / 'package.json')
+pkg_json = parse_json(project_folder / "package.json")
+pkg_json_rxvdom = parse_json(project_folder / ".." / "package.json")
 # (cd ./node_modules/@youwol/mkdocs-ts/bin/ && node index.js --project ../../../../.. --nav /api --out ../../../../assets/api)
 externals_deps = {
     "mkdocs-ts": "^0.3.0",
     "rx-vdom": f"^{pkg_json_rxvdom['version'].replace('-wip', '')}",
     "@w3nest/webpm-client": "^0.1.2",
-    "rxjs": "^7.5.6"
+    "rxjs": "^7.5.6",
 }
 in_bundle_deps = {}
 dev_deps = {
@@ -24,40 +31,32 @@ dev_deps = {
     # typing of VirtualDOM.
     "@w3nest/rx-code-mirror-editors": "^0.1.0",
     "ts-node": "^10.9.2",
-    "tsconfig-paths": "^4.2.0"
+    "tsconfig-paths": "^4.2.0",
 }
 
 config = ProjectConfig(
     path=project_folder,
     type=PackageType.APPLICATION,
-    name=pkg_json['name'],
-    version=pkg_json_rxvdom['version'],
-    shortDescription=pkg_json['description'],
-    author=pkg_json['author'],
+    name=pkg_json["name"],
+    version=pkg_json_rxvdom["version"],
+    shortDescription=pkg_json["description"],
+    author=pkg_json["author"],
     dependencies=Dependencies(
-        runTime=RunTimeDeps(
-            externals=externals_deps,
-            includedInBundle=in_bundle_deps
-        ),
-        devTime=dev_deps
+        runTime=RunTimeDeps(externals=externals_deps, includedInBundle=in_bundle_deps),
+        devTime=dev_deps,
     ),
     bundles=Bundles(
-         mainModule=MainModule(
-             entryFile='./main.ts',
-             loadDependencies=list(externals_deps.keys())
-         )
+        mainModule=MainModule(
+            entryFile="./main.ts", loadDependencies=list(externals_deps.keys())
+        )
     ),
-    devServer=DevServer(
-        port=3027
-    ),
+    devServer=DevServer(port=3027),
     inPackageJson={
-        "scripts" :{
-            "doc": "npx tsx .w3nest/doc.ts"
-        },
-    }
+        "scripts": {"doc": "npx tsx .w3nest/doc.ts"},
+    },
 )
 
-template_folder = project_folder / '.w3nest' / '.template'
+template_folder = project_folder / ".w3nest" / ".template"
 generate_template(config=config, dst_folder=template_folder)
 
 files = [
@@ -69,6 +68,6 @@ files = [
     "package.json",
     "jest.config.ts",
     "webpack.config.ts",
-    ]
+]
 for file in files:
     copyfile(src=template_folder / file, dst=project_folder / file)
