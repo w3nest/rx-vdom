f992752f4597227108593839aa6d72543efe2621
youwol-guillaume <reinisch.gui@gmail.com>
2025-04-08 06:21:07 +0200

    📝 Sync. webpm `script.src`  import

--- doc/src/app/main.ts
+++ doc/src/app/main.ts
@@ -8,7 +8,7 @@ await setup.installMainModule({
     cdnClient: webpmClient,
     installParameters: {
         css: [
-            'bootstrap#5.3.0~bootstrap.min.css',
+            'bootstrap#5.3.3~bootstrap.min.css',
             'fontawesome#5.12.1~css/all.min.css',
             `mkdocs-ts#${setup.runTimeDependencies.externals['mkdocs-ts']}~assets/mkdocs-light.css`,
             `mkdocs-ts#${setup.runTimeDependencies.externals['mkdocs-ts']}~assets/notebook.css`,
