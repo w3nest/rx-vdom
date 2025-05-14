2f20aa0c3eae93a710626fc9a701445c2b360318
youwol-guillaume <reinisch.gui@gmail.com>
2025-05-14 07:07:24 +0200

    👽️ [doc-app] use `mkdocs-ts` to define links

--- doc/src/app/config.markdown.ts
+++ doc/src/app/config.markdown.ts
@@ -1,13 +1,6 @@
 import pkgJson from '../../package.json'
 import { DefaultLayout, fromMarkdown, GlobalMarkdownViews } from 'mkdocs-ts'
-import {
-    ExtLink,
-    rxVDomSize,
-    exampleHome,
-    apiLink,
-    CrossLink,
-    GitHubLink,
-} from './md-widgets'
+import { rxVDomSize, exampleHome } from './md-widgets'
 import { example1 } from './js-plaground-examples'
 import { companionNodes$ } from './on-load'
 
@@ -17,12 +10,6 @@ GlobalMarkdownViews.factory = {
     ...GlobalMarkdownViews.factory,
     'rx-vdom-size': () => rxVDomSize(),
     'example-home': () => exampleHome(),
-    'api-link': (elem: HTMLElement) => {
-        return apiLink(elem)
-    },
-    'ext-link': (elem: HTMLElement) => new ExtLink(elem),
-    'cross-link': (elem: HTMLElement) => new CrossLink(elem),
-    'github-link': (elem: HTMLElement) => new GitHubLink(elem),
     'split-api': () => ({
         tag: 'i',
         class: 'mkdocs-inv',
--- doc/src/app/md-widgets.ts
+++ doc/src/app/md-widgets.ts
@@ -1,13 +1,9 @@
-import {
-    AnyVirtualDOM,
-    attr$,
-    ChildrenLike,
-    CSSAttribute,
-    VirtualDOM,
-} from 'rx-vdom'
+import { AnyVirtualDOM, attr$ } from 'rx-vdom'
 import pkgJson from '../../package.json'
 import { from, map, timer } from 'rxjs'
 import { MdWidgets } from 'mkdocs-ts'
+
+import LinksDict from './links.json'
 
 export function rxVDomSize(): AnyVirtualDOM {
     const url = `/api/assets-gateway/webpm/resources/${window.btoa('rx-vdom')}/${pkgJson.version}/dist/rx-vdom.js`
@@ -50,184 +46,26 @@ export function exampleHome(): AnyVirtualDOM {
         ],
     }
 }
-
-export function apiLink(elem: HTMLElement): AnyVirtualDOM {
-    const target = elem.getAttribute('target')
+MdWidgets.ApiLink.Mapper = (target: string, elem: HTMLElement) => {
     const kind = elem.getAttribute('kind')
-    if (!target) {
-        throw Error('Target is not specified')
-    }
     return {
-        tag: 'a',
         href: `@nav/api.${target}`,
-        class: `mkapi-semantic-flag mkapi-role-${kind ?? ''}`,
-        children: [
-            {
-                tag: 'i',
-                innerText: target,
-            },
-            {
-                tag: 'i',
-                class: 'fas fa-code',
-                style: {
-                    transform: 'scale(0.65) translate(0,-7px)',
-                },
-            },
-        ],
+        withClass: `mkapi-role-${kind ?? ''}`,
+    }
+}
+MdWidgets.ExtLink.Mapper = (target: string) => {
+    return {
+        href: LinksDict.extLinks[target] as string,
+    }
+}
+MdWidgets.GitHubLink.Mapper = (target: string) => {
+    return {
+        href: LinksDict.githubLinks[target] as string,
     }
 }
 
-export class ExtLink implements VirtualDOM<'a'> {
-    public readonly tag = 'a'
-    public readonly children: ChildrenLike
-    public readonly innerText: string
-    public readonly href: string
-    public readonly target = '_blank'
-
-    constructor(elem: HTMLElement) {
-        const target = elem.getAttribute('target')
-        if (!target) {
-            console.warn('Can no find target for extlink', elem)
-            return
-        }
-        const navs = {
-            rxjs: 'https://rxjs.dev/',
-            rxjsOperators: 'https://rxjs.dev/guide/operators',
-            react: 'https://react.dev',
-            vue: 'https://vuejs.org',
-            learnRxJS: 'https://www.learnrxjs.io/',
-            mkdocsTS: '/apps/@mkdocs-ts/doc/latest',
-            reactivex: 'https://reactivex.io/',
-            chartjs: 'https://www.chartjs.org/',
-            shareReplay: 'https://rxjs.dev/api/operators/shareReplay',
-        }
-        if (!(target in navs)) {
-            return
-        }
-        this.href = navs[target as keyof typeof navs]
-        this.children = [
-            {
-                tag: 'i',
-                innerText: elem.textContent ?? '',
-            },
-            {
-                tag: 'i',
-                class: 'fas fa-external-link-alt',
-                style: { transform: 'scale(0.6)' },
-            },
-        ]
-    }
-}
-
-export class GitHubLink implements VirtualDOM<'a'> {
-    public readonly tag = 'a'
-    public readonly children: ChildrenLike
-    public readonly innerText: string
-    public readonly href: string
-    public readonly target = '_blank'
-
-    constructor(elem: HTMLElement) {
-        const target = elem.getAttribute('target')
-
-        if (!target) {
-            return
-        }
-        const navs = {
-            'rx-vdom': 'https://github.com/w3nest/rx-vdom',
-            examples: 'https://github.com/w3nest/rx-vdom/tree/main/examples',
-        }
-        if (!(target in navs)) {
-            return
-        }
-        this.href = navs[target as keyof typeof navs]
-        this.children = [
-            {
-                tag: 'i',
-                innerText: elem.textContent ?? '',
-            },
-            {
-                tag: 'i',
-                class: 'fab fa-github',
-                style: { transform: 'scale(0.8)' },
-            },
-        ]
-    }
-}
-
-export class CrossLink implements VirtualDOM<'a'> {
-    public readonly tag = 'a'
-    public readonly children: ChildrenLike
-    public readonly innerText: string
-    public readonly href: string
-
-    constructor(elem: HTMLElement) {
-        const target = elem.getAttribute('target')
-        if (!target) {
-            return
-        }
-        const navs = {
-            gettingStarted: '@nav/tutorials/basics',
-            todoApp: '@nav/tutorials/todo',
-            howTo: '@nav/how-to',
-            api: '@nav/api',
-        }
-        if (!(target in navs)) {
-            return
-        }
-        this.href = navs[target as keyof typeof navs]
-        this.children = [
-            {
-                tag: 'i',
-                innerText: elem.textContent ?? '',
-            },
-            {
-                tag: 'i',
-                class: 'fas fa-book-open',
-                style: { transform: 'scale(0.6)' },
-            },
-        ]
-    }
-}
-
-export class NavHeaderView implements VirtualDOM<'div'> {
-    public readonly tag = 'div'
-    public readonly class = 'd-flex align-items-center justify-content-center'
-    public readonly children: ChildrenLike
-    public readonly style: CSSAttribute
-
-    constructor(params: { topStickyPaddingMax: string }) {
-        this.style = {
-            height: params.topStickyPaddingMax,
-        }
-        this.children = [
-            {
-                tag: 'a',
-                class: 'mx-2',
-                target: '_blank',
-                href: 'https://github.com/w3nest/rx-vdom',
-                children: [
-                    {
-                        ...MdWidgets.githubIcon,
-                        style: {
-                            filter: 'invert(1)',
-                        },
-                    },
-                ],
-            },
-            {
-                tag: 'a',
-                class: 'mx-2',
-                target: '_blank',
-                href: 'https://www.npmjs.com/package/rx-vdom',
-                children: [MdWidgets.npmIcon],
-            },
-            {
-                tag: 'a',
-                class: 'mx-2',
-                target: '_blank',
-                href: 'https://github.com/w3nest/rx-vdom/blob/main/doc/LICENSE',
-                children: [MdWidgets.mitIcon],
-            },
-        ]
+MdWidgets.CrossLink.Mapper = (target: string) => {
+    return {
+        href: LinksDict.githubLinks[target] as string,
     }
 }
