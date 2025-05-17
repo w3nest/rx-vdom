87c14a4d31461b3d3a27c65daf9f610253c9055d
youwol-guillaume <reinisch.gui@gmail.com>
2025-05-17 09:35:24 +0200

    👽️ [examples] sync. todo-app JS & TS

--- examples/todo-app-ts/.w3nest/setup.py
+++ examples/todo-app-ts/.w3nest/setup.py
@@ -1,59 +1,58 @@
 import shutil
 from pathlib import Path
 
-from w3nest.ci.ts_frontend import ProjectConfig, PackageType, Dependencies, \
-    RunTimeDeps, generate_template, DevServer, Bundles, MainModule
+from w3nest.ci.ts_frontend import (
+    ProjectConfig,
+    PackageType,
+    Dependencies,
+    RunTimeDeps,
+    generate_template,
+    DevServer,
+    Bundles,
+    MainModule,
+)
 from w3nest.utils import parse_json
 
 project_folder = Path(__file__).parent.parent
 
-pkg_json = parse_json(project_folder / 'package.json')
+pkg_json = parse_json(project_folder / "package.json")
 
 load_dependencies = {
-    "rxjs": "^7.5.6",
-    "rx-vdom": "^0.1.1",
-    "@w3nest/webpm-client": "^0.1.2"
+    "rxjs": "^7.8.2",
+    "rx-vdom": "^0.1.5",
+    "@w3nest/webpm-client": "^0.1.8",
 }
 
 config = ProjectConfig(
     path=project_folder,
     type=PackageType.APPLICATION,
-    name=pkg_json['name'],
-    version=pkg_json['version'],
-    shortDescription=pkg_json['description'],
-    author=pkg_json['author'],
-    dependencies=Dependencies(
-        runTime=RunTimeDeps(
-            externals=load_dependencies
-        )
-    ),
-    devServer=DevServer(
-        port=4001
-    ),
+    name=pkg_json["name"],
+    version=pkg_json["version"],
+    shortDescription=pkg_json["description"],
+    author=pkg_json["author"],
+    dependencies=Dependencies(runTime=RunTimeDeps(externals=load_dependencies)),
+    devServer=DevServer(port=4001),
     bundles=Bundles(
         mainModule=MainModule(
-            entryFile='./index.ts',
-            loadDependencies=list(load_dependencies.keys())
+            entryFile="./app/main.ts", loadDependencies=list(load_dependencies.keys())
         )
-
-    )
+    ),
 )
 
 
-template_folder = project_folder / '.w3nest' / '.template'
+template_folder = project_folder / ".w3nest" / ".template"
 
 generate_template(config=config, dst_folder=template_folder)
 
 files = [
-    Path("src") / "auto-generated.ts",
-    'README.md',
-    '.gitignore',
-    '.npmignore',
-    '.prettierignore',
-    'package.json',
-    'jest.config.ts',
-    'tsconfig.json',
-    'webpack.config.ts'
-    ]
+    "README.md",
+    ".gitignore",
+    ".npmignore",
+    ".prettierignore",
+    "package.json",
+    "jest.config.ts",
+    "tsconfig.json",
+    "webpack.config.ts",
+]
 for file in files:
     shutil.copyfile(src=template_folder / file, dst=project_folder / file)
--- examples/todo-app-ts/src/app/main.ts
+++ examples/todo-app-ts/src/app/main.ts
@@ -1,21 +1,28 @@
 import './style.css'
 export {}
-import * as webpmClient from '@w3nest/webpm-client'
-import { setup } from '../auto-generated'
+import { install, LoadingScreen } from '@w3nest/webpm-client'
+import pkgJson from '../../package.json'
+
+const loadingScreen = new LoadingScreen({
+    logo: '../assets/favicon.svg',
+    name: pkgJson.name,
+    description: pkgJson.description,
+})
 
 /**
  *  To take advantage of YouWol ecosystem, the dependencies are not included in the bundle but are kept 'externals'.
  *  This file handle the actual installation of them (if needed, they will most likely already be cached by the browser).
  */
-await setup.installMainModule({
-    cdnClient: webpmClient,
-    installParameters: {
-        css: [
-            'bootstrap#5.3.3~bootstrap.min.css',
-            'fontawesome#5.12.1~css/all.min.css',
-        ],
-        displayLoadingScreen: true,
+await install({
+    esm: [`${pkgJson.name}#${pkgJson.version}`],
+    css: [
+        'bootstrap#5.3.3~bootstrap.min.css',
+        'fontawesome#5.12.1~css/all.min.css',
+    ],
+    onEvent: (ev) => {
+        loadingScreen.next(ev)
     },
 })
 
 await import('./on-load')
+loadingScreen.done()
